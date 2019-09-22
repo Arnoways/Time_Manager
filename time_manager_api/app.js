18a6a69f1109de87028dd99859cc8cf01be42a1f
arnaud <arnaud.sinays@epitech.eu>
2019-09-22 13:09:47 +0200

    feat: swagger metrics

--- time_manager_api/app.js
+++ time_manager_api/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+/* swagger stuff */
+var swStats = require('swagger-stats');
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger.json')
 
+/* route stuff */
 var indexRouter = require('./routes/index');
 var employeesRouter = require('./routes/employee');
 var clocksRouter = require('./routes/clock');
@@ -21,7 +25,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(swStats.getMiddleware({swaggerSpec: swaggerDocument}));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
